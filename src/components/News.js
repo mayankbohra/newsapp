a514321c2e8e99a1b6c158cb54b57dbd9c0a14cd
mayankbohra <bohramayank4u@gmail.com>
2023-05-27 11:31:03 +0530

    add news component

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        Test News component
+      </div>
+    )
+  }
+}
+
+export default News
